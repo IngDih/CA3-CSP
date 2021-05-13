8609572b574558caea91f68fe58ec25bec1b31fe
Ingrid Castro <72034663+IngDih@users.noreply.github.com>
2021-05-13 03:49:01 +0100

    Menu table basics

--- js/password.js
+++ js/password.js
@@ -8,17 +8,6 @@ function checkPasswordLength(){
     }
 }
 
-function checkPasswordUppercase(){
-    var password = el.value;
-    if (password.contains < 8){
-        elMsg.className = 'warning';
-        elMsg.textContent = 'Not an ideal password yet.'
-    } else {
-        el.Msg.textContent = '';
-    }
-}
-
-
 function tipUsername() {
     elMsg.className = 'tip';
     elMsg.innerHTML = 'Your password should contain at least 8 characters, including: *one lowercase letter; *One uppercase letter; *One digit; *One special character.';    
