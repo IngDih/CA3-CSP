6a82fd0cf998a476b0df4f5164d99c10e7ce69a4
Ingrid Castro <72034663+IngDih@users.noreply.github.com>
2021-05-14 19:16:04 +0100

    backup

--- js/switchbgcolour.js
+++ js/switchbgcolour.js
@@ -1,7 +1,9 @@
+/* Switch colour button on page 2 */
+
 var color = ["#caf7e3", "#b6b6d9", "#FFD2DD", "#FAFFDB", "#DBE9FF", "#FFDBDB"]
-                var i = 0;
-                document.querySelector("button").addEventListener("click",
-                    function () {
-                        i = i < color.length ? ++i : 0;
-                        document.querySelector("body").style.background = color[i]
-                    })
+var i = 0;
+document.querySelector(".button3").addEventListener("click",
+    function () {
+        i = i < color.length ? ++i : 0;
+        document.querySelector("body").style.background = color[i]
+    })
