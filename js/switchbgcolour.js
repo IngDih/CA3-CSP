330c1554180ac6b388ad4a61c069719f8d67af58
Ingrid Castro <72034663+IngDih@users.noreply.github.com>
2021-05-14 17:32:31 +0100

    code fixes + pg2 colums + switchbgcolour.js added

---
+++ js/switchbgcolour.js
@@ -0,0 +1,7 @@
+var color = ["#caf7e3", "#b6b6d9", "#FFD2DD", "#FAFFDB", "#DBE9FF", "#FFDBDB"]
+                var i = 0;
+                document.querySelector("button").addEventListener("click",
+                    function () {
+                        i = i < color.length ? ++i : 0;
+                        document.querySelector("body").style.background = color[i]
+                    })
